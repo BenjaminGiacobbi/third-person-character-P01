f4b65d391551b777d6ffaa914bee00073a988ffc
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-08-24 17:13:03 -0500

    Third Person Controller
    Based on Brackeys Tutorial

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 6f;
+    [SerializeField] float _turnSmoothTime = 0.1f;
+
+    CharacterController _controller;
+    Transform _camTransform;
+    private float turnSmoothVelocity;
+
+    // caching
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _camTransform = Camera.main.transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            // Atan is tangent of angle between the x axis and vector starting at 0 and terminating at x, y (in radians by default)
+            // passing in x, then z adjusts for the forward direction being positive z here
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camTransform.eulerAngles.y;
+
+            // SmoothDampAngle adjusts and smooths the turn
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, _turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // adjusts the direction of movement by applying the forward direction to the player's quaternion rotation of targetAngle
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _controller.Move(moveDirection.normalized * _speed * Time.deltaTime);
+        }
+    }
+}
