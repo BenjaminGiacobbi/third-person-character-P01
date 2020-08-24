528645ff75ad3e6afbc7290c54bd257d1c2ce51f
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-08-24 18:57:53 -0500

    Character Controller
    Mostly Finished

---
+++ Assets/PlayerCharacterAnimator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(ThirdPersonMovement))]     
+public class PlayerCharacterAnimator : MonoBehaviour
+{
+    // these names are the same as the animation nodes in Mecanim
+    const string IdleState = "Idle";
+    const string RunState = "Run";
+    const string JumpState = "Jumping";
+    const string FallState = "Falling";
+
+    // animator field
+    Animator _animator = null;
+    ThirdPersonMovement _movementScript = null;
+
+    // caching
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _movementScript = GetComponent<ThirdPersonMovement>(); // some workflows might have the animator as a child object, so 
+                                                               // keep in mind that this might be filled as an inspector reference
+    }
+
+
+    #region subscriptions
+    private void OnEnable()
+    {
+        _movementScript.Idle += OnIdle;
+        _movementScript.StartRunning += OnStartRunning;
+    }
+
+    private void OnDisable()
+    {
+        _movementScript.Idle -= OnIdle;
+        _movementScript.StartRunning -= OnStartRunning;
+    }
+    #endregion
+
+    // i have no idea what any of this I'm trying my best alright
+    public void OnIdle()
+    {
+        _animator.CrossFadeInFixedTime(IdleState, .2f);
+    }
+
+    public void OnStartRunning()
+    {
+        _animator.CrossFadeInFixedTime(RunState, .2f);
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(CharacterController))]
 public class ThirdPersonMovement : MonoBehaviour
 {
+    public event Action Idle = delegate { };
+    public event Action StartRunning = delegate { };
+
     [SerializeField] float _speed = 6f;
     [SerializeField] float _turnSmoothTime = 0.1f;
 
     CharacterController _controller;
     Transform _camTransform;
-    private float turnSmoothVelocity;
+    private float _turnSmoothVelocity;
+    private bool _isMoving = false;
 
     // caching
     private void Awake()
@@ -19,6 +24,11 @@ public class ThirdPersonMovement : MonoBehaviour
         _camTransform = Camera.main.transform;
     }
 
+    // start is called the frame after awake
+    private void Start()
+    {
+        Idle?.Invoke();
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,17 +40,46 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
+            CheckIfStartedMoving();
             // Atan is tangent of angle between the x axis and vector starting at 0 and terminating at x, y (in radians by default)
             // passing in x, then z adjusts for the forward direction being positive z here
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camTransform.eulerAngles.y;
 
             // SmoothDampAngle adjusts and smooths the turn
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, _turnSmoothTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             // adjusts the direction of movement by applying the forward direction to the player's quaternion rotation of targetAngle
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             _controller.Move(moveDirection.normalized * _speed * Time.deltaTime);
         }
+
+        else
+        {
+            CheckIfStoppedMoving();
+        }
+    }
+
+
+    private void CheckIfStartedMoving()
+    {
+        if(_isMoving == false)
+        {
+            // our velocity said we started moving but previously we were not, so set _isMoving
+            StartRunning?.Invoke();
+            Debug.Log("Started");
+        }
+        _isMoving = true;
+    }
+
+    private void CheckIfStoppedMoving()
+    {
+        if (_isMoving == true)
+        {
+            // our velocity said we stopped moving but previously were, so set _isMoving
+            Idle?.Invoke();
+            Debug.Log("Stopped");
+        }
+        _isMoving = false;
     }
 }
