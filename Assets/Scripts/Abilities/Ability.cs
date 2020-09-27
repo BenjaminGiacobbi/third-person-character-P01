4dc289a5fc8366b00a2c0df054e8efc25391f5c6
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-09-27 18:14:48 -0500

    Final Changes 2 (Electric Boogaloo)
    Tons of minor stuff, such as changing some sound effects, fixing cure's particles, editing ALL particles, etc

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Abilities")]
+public abstract class Ability : ScriptableObject
+{
+    public string abilityName;
+    public Color abilityColor;
+    public Sprite abilitySprite;
+    public float castTime;
+    public float duration;
+    public float cooldown;
+    public AudioClip startSound;
+    public AudioClip activeSound;
+
+    public abstract void Setup();
+    public abstract void Use(Transform origin, Transform target);
+}
---
+++ Assets/Scripts/Abilities/Cure.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cure")]
+public class Cure : Ability
+{
+    [SerializeField] int _healAmount = 5;
+
+    public override void Setup()
+    {
+
+    }
+
+    public override void Use(Transform origin, Transform target)
+    {
+        if (target == null)
+        {
+            Debug.Log("Cannot cast cure without a target!");
+            return;
+        }
+            
+        // searches both objects just in case
+        target.GetComponent<Health>()?.Heal(_healAmount);
+        target.GetComponentInParent<Health>()?.Heal(_healAmount);
+        AudioHelper.PlayClip2D(startSound, 0.35f);
+    }
+}
